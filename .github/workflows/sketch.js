48091ada795284040fe57e04d4a9e7027edfa7d8
WDKpangzi <18600207212m0@gmail.com>
2025-10-01 18:28:06 -0400

    add p5 file with GIF

---
+++ .github/workflows/sketch.js
@@ -0,0 +1,17 @@
+let failGif;
+
+function preload()
+{
+failGif = loadImage("fail•g¡f");
+}
+
+function setup()
+{
+createCanvas (400,800);
+}
+
+function drawl()
+{
+background (0); 
+image (failG1f,0,0);
+}
